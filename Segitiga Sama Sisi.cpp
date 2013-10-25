b9e4e06df3ac3a712d23edc1b6bfb7f7b7fddf5b
SharKing-Lin <suv_asfly@yahoo.com>
2013-10-25 19:03:10 +0700

    Update Segitiga Sama Sisi.cpp

--- Segitiga Sama Sisi.cpp
+++ Segitiga Sama Sisi.cpp
@@ -3,6 +3,12 @@
 
 void main()
 {
+	printf("\t\t\t\t========================\n");
+        printf("\t\t\t\t|| %c%c%c 1701313990 %c%c%c ||\n", 6,3,5,5,3,6);
+        printf("\t\t\t\t||         **         ||\n");
+        printf("\t\t\t\t||  %c   SharKing   %c  ||\n", 6,6);
+        printf("\t\t\t\t========================\n\n\n");
+	
 	int a = 1, b = 0, c = 0, x, d = 1;
 	printf("Input how much you want to show the * : ");
 	scanf("%d", &x); fflush(stdin);
