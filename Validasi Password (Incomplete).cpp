c20a3d90182d64edcb517c55d6d17891967594d3
SharKing-Lin <suv_asfly@yahoo.com>
2013-10-25 20:37:02 +0700

    Create Validasi Password (Incomplete).cpp
    Membuat validasi username password dengan strcmp, dan strcmpi.

---
+++ Validasi Password (Incomplete).cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{
+	printf("\t\t\t\t========================\n");
+	printf("\t\t\t\t|| %c%c%c 1701313990 %c%c%c ||\n", 6,3,5,5,3,6);
+	printf("\t\t\t\t||         **         ||\n");
+	printf("\t\t\t\t||  %c   SharKing   %c  ||\n", 6,6);
+	printf("\t\t\t\t========================\n\n\n");
+}
