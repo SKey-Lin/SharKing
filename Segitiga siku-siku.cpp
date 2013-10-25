37f866f17466ff759089f49aac6972cdf509b5dd
SharKing-Lin <suv_asfly@yahoo.com>
2013-10-25 19:02:52 +0700

    Update Segitiga siku-siku.cpp

--- Segitiga siku-siku.cpp
+++ Segitiga siku-siku.cpp
@@ -3,6 +3,12 @@
 
 void main()
 {
+	printf("\t\t\t\t========================\n");
+        printf("\t\t\t\t|| %c%c%c 1701313990 %c%c%c ||\n", 6,3,5,5,3,6);
+        printf("\t\t\t\t||         **         ||\n");
+        printf("\t\t\t\t||  %c   SharKing   %c  ||\n", 6,6);
+        printf("\t\t\t\t========================\n\n\n");
+	
 	int i = 0, j = 0, n, k=0;
 	printf("Input how much you want to show the * : ");
 	scanf("%d", &n);
