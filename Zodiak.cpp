88fa2ce60a358eff95d2568de7ca9f801c9fde9f
SharKing-Lin <suv_asfly@yahoo.com>
2013-10-25 18:30:57 +0700

    Update and rename zodiak.cpp to Zodiak.cpp

--- Zodiak.cpp
+++ Zodiak.cpp
@@ -7,7 +7,7 @@ void main()
 	printf("\t\t\t\t|| %c%c%c 1701313990 %c%c%c ||\n", 6,3,5,5,3,6);
 	printf("\t\t\t\t||         **         ||\n");
 	printf("\t\t\t\t||  %c   SharKing   %c  ||\n", 6,6);
-	printf("\t\t\t\t========================\n");
+	printf("\t\t\t\t========================\n\n\n");
 	printf("Pilihlah zodiak kalian: \n");
 	printf("A. Aries\n");
 	printf("B. Taurus\n");
