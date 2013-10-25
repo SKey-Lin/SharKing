d0f7180837d8ee960c54407bd5a79614d14d099a
SharKing-Lin <suv_asfly@yahoo.com>
2013-10-25 18:30:25 +0700

    Update Grade.cpp

--- Grade.cpp
+++ Grade.cpp
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
+	printf("\t\t\t\t========================\n");
+	printf("\t\t\t\t|| %c%c%c 1701313990 %c%c%c ||\n", 6,3,5,5,3,6);
+	printf("\t\t\t\t||         **         ||\n");
+	printf("\t\t\t\t||  %c   SharKing   %c  ||\n", 6,6);
+	printf("\t\t\t\t========================\n\n\n");
+	
 	char nama[100];
 	printf("Input Your Name :");
 	scanf("%[^\n]",&nama);
