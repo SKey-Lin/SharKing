e928098c6842a9b85bd44d534615b741f372e5d8
SharKing-Lin <suv_asfly@yahoo.com>
2013-10-28 13:04:49 +0700

    Update Diamond (Incomplete).cpp

--- Diamond (Incomplete).cpp
+++ Diamond (Incomplete).cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 
 
 void main()
@@ -8,44 +8,55 @@ void main()
 	printf("\t\t\t\t||         **         ||\n");
 	printf("\t\t\t\t||  %c   SharKing   %c  ||\n", 6,6);
 	printf("\t\t\t\t========================\n\n\n");
-	
-	int a = 1, b = 0, c = 0, x, d = 0;
-        printf("Input how much you want to show the * : ");
-        scanf("%d", &x); fflush(stdin);
-        while (a <= x)
-        {
-                for(b =0; b < (x - d); b++)
-                {
-                        printf(" ");
-                }
-               
-                for ( c = 0;c < (a-1); c++)
-                {
-                        printf("*");
-                }
-                printf("\n");
-                a++;
-                d++;
-        }
-        
-        a = 1; b = 0; c = 0; d = 0;
-        while (a <= x)
-        {
-                for ( c = 0;c < (a-1); c++)
-                {
-                        printf(" ");
-                }
-                for(b =0; b < (x - d); b++)
-                {
-                        printf("*");
-                }
-               
-                
-                printf("\n");
-                a++;
-                d++;
-        }
-        
-        getchar();
-	
+
+	int x, a=0, b=1, j, k, i;
+	printf("Input the *: ");
+	scanf("%d", &x); fflush(stdin);
+	for(i=1; i <= x/2; i++)
+	{
+		for(k=1; k <= (x-b)/2; k++)
+		{
+			printf(" ");
+		}
+		for(j=1; j <= i+a; j++)
+		{
+			printf("*");
+		}
+		b+=2;
+		a++;
+		printf("\n");
+	}
+	if(x%2==0)
+	{
+		for(i=1; i <= (x/2)-1;i++)
+		{
+			for(k=1; k <= i; k++)
+			{
+				printf(" ");
+			}
+			for(j=1; j <= i+a; j++)
+			{
+				printf("*");
+			}
+			a-=3;
+			printf("\n");
+		}
+	}
+	else
+	{
+		for(i=1; i <= (x/2)-1;i++)
+		{
+			for(k=1; k <= i+1; k++)
+			{
+				printf(" ");
+			}
+			for(j=1; j <= i+a-2; j++)
+			{
+				printf("*");
+			}
+			a-=3;
+			printf("\n");
+		}
+	}
+	getchar();
 }
