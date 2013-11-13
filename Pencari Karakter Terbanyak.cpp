4994210c586954137871db31c2b1ae1b8bce4a27
SharKing-Lin <suv_asfly@yahoo.com>
2013-11-13 21:19:57 +0700

    Update Pencari Karakter Terbanyak.cpp

--- Pencari Karakter Terbanyak.cpp
+++ Pencari Karakter Terbanyak.cpp
@@ -3,6 +3,12 @@
 
 void main()
 {
+	printf("\t\t\t\t========================\n");
+	printf("\t\t\t\t|| %c%c%c 1701313990 %c%c%c ||\n", 6,3,5,5,3,6);
+	printf("\t\t\t\t||         **         ||\n");
+	printf("\t\t\t\t||  %c   SharKing   %c  ||\n", 6,6);
+	printf("\t\t\t\t========================\n\n\n");
+
 	int i, j, a=0, max=0;
 	char kalimat[100], x;
 	printf("Input the sentences: ");
